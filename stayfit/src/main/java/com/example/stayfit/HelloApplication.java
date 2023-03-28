b5b8d7bea293efa09709d85e2d88d86710d446d0
Armin1503 <arminsalkovic1503@gmail.com>
2023-03-28 12:20:04 +0200

    created navbar

--- stayfit/src/main/java/com/example/stayfit/HelloApplication.java
+++ stayfit/src/main/java/com/example/stayfit/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.stayfit;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.getIcons().add(new Image(HelloApplication.class.getResourceAsStream("logo.jpg")));
+
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+
+        stage.setTitle("Stayfit");
         stage.setScene(scene);
         stage.show();
     }

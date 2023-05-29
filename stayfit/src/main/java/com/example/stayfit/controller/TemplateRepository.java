8ece45fe3a8b873f6fc79f3bbce8998aefae711a
AbdlAl <abdubed2@gmail.com>
2023-05-29 23:57:26 +0200

    feat: new features
    - finished TemplateRepository

--- stayfit/src/main/java/com/example/stayfit/controller/TemplateRepository.java
+++ stayfit/src/main/java/com/example/stayfit/controller/TemplateRepository.java
@@ -1,4 +1,151 @@
 package com.example.stayfit.controller;
 
-public class TemplateRepository {
+import com.example.stayfit.entity.Template;
+import com.example.stayfit.entity.User;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TemplateRepository implements Persistent<Template> {
+
+    private DataSource dataSource = Database.getDataSource();
+    @Override
+    public void save(Template template) {
+        if(template.getId() == null){
+            insert(template);
+        }else{
+            update(template);
+        }
+
+    }
+
+    @Override
+    public void insert(Template template) {
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "INSERT INTO S_TEMPLATE(USER_ID,TEMPLATE_NAME) VALUES (?,?)";
+
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setLong(1, template.getUser().getId());
+            statement.setString(2, template.getName());
+
+            if (statement.executeUpdate() == 0) {
+                throw new SQLException("Insert of Template failed, no rows affected");
+            }
+
+
+            try (ResultSet keys = statement.getGeneratedKeys()) {
+                if (keys.next()) {
+                    template.setId(keys.getLong(1));
+                } else {
+                    throw new SQLException("Insert into Template failed, no ID obtained");
+                }
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(Template template) {
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "DELETE FROM S_TEMPLATE WHERE TEMPLATE_NR=?";
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setLong(1,template.getId() );
+
+            if (statement.executeUpdate() == 0) {
+                throw new SQLException("Delete from Template failed, no rows affected");
+            }
+            template.setId(null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Template> findAll() {
+        List<Template> templateList = new ArrayList<>();
+
+
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "SELECT * FROM S_TEMPLATE";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()) {
+                UserRepository userRepository = new UserRepository();
+
+                Long id = result.getLong("TEMPLATE_NR");
+                String name = result.getString("TEMPLATE_NAME");
+                User user = userRepository.findById(result.getLong("USER_ID"));
+
+                Template template = new Template(user, name);
+                template.setId(id);
+
+                templateList.add(template);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return templateList;
+    }
+
+    @Override
+    public Template findById(Long id) {
+
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "SELECT * FROM S_TEMPLATE WHERE TEMPLATE_NR=?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setLong(1, id);
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()) {
+                if(id == result.getInt("TEMPLATE_NR")) {
+                    UserRepository userRepository = new UserRepository();
+
+                    String name = result.getString("TEMPLATE_NAME");
+
+                    Template template = new Template(userRepository.findById(result.getLong("USER_ID")), name);
+                    template.setId(id);
+
+                    return template;
+                }
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void update(Template template) {
+
+        try (Connection connection = dataSource.getConnection()) {
+            String sql = "UPDATE S_TEMPLATE SET USER_ID=?,TEMPLATE_NAME=? WHERE TEMPLATE_NR=?";
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setLong(1, template.getUser().getId());
+            statement.setString(2, template.getName());
+            statement.setLong(3, template.getId());
+
+
+            if (statement.executeUpdate() == 0) {
+                throw new SQLException("Update of Template failed, no rows affected");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- stayfit/src/main/java/com/example/stayfit/controller/UserRepository.java
+++ stayfit/src/main/java/com/example/stayfit/controller/UserRepository.java
@@ -1,4 +1,37 @@
 package com.example.stayfit.controller;
 
-public class UserRepository {
+import com.example.stayfit.entity.User;
+
+import java.util.List;
+
+public class UserRepository implements Persistent {
+    @Override
+    public void save(Object entity) {
+
+    }
+
+    @Override
+    public void insert(Object entity) {
+
+    }
+
+    @Override
+    public void delete(Object id) {
+
+    }
+
+    @Override
+    public List findAll() {
+        return null;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void update(Object entity) {
+
+    }
 }

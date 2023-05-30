1fbf65c5023a333541502136a116a5a84d5d27fd
AbdlAl <abdubed2@gmail.com>
2023-05-30 11:11:51 +0200

    Finished ExercisePosition and Set tests

--- stayfit/src/test/java/com/example/stayfit/entity/SetTest.java
+++ stayfit/src/test/java/com/example/stayfit/entity/SetTest.java
@@ -1,4 +1,40 @@
 package com.example.stayfit.entity;
 
+import com.example.stayfit.model.entity.Exercise;
+import com.example.stayfit.model.entity.Set;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SetTest {
+    @BeforeEach
+    public void setUp(){
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+    }
+
+
+    @Test
+    void createSet(){
+        //arrange
+        Exercise exercise = new Exercise();
+        Long weight = 80L;
+        Long reps = 10L;
+
+        Set set = new Set(exercise, weight, reps);
+
+        //act
+
+        //assert
+        assertThat(set.getId()).isNull();
+        assertThat(set.getExercise().getId()).isNull();
+        assertThat(set.getWeight()).isEqualTo(80L);
+        assertThat(set.getReps()).isEqualTo(10L);
+    }
 }

eb292d068d914c1eace1770fcd05d7edb1d7f194
AbdlAl <abdubed2@gmail.com>
2023-05-30 10:42:31 +0200

    Finished ExercisePosition and Set tests

--- stayfit/src/test/java/com/example/stayfit/entity/ExercisePositionTest.java
+++ stayfit/src/test/java/com/example/stayfit/entity/ExercisePositionTest.java
@@ -1,4 +1,41 @@
 package com.example.stayfit.entity;
 
+import com.example.stayfit.model.entity.Exercise;
+import com.example.stayfit.model.entity.ExercisePosition;
+import com.example.stayfit.model.entity.Template;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 public class ExercisePositionTest {
+    @BeforeEach
+    public void setUp(){
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+    }
+
+
+    @Test
+    void createExercisePosition(){
+        //arrange
+        Template template = new Template();
+        Exercise exercise = new Exercise();
+
+        ExercisePosition exercisePosition = new ExercisePosition(exercise, template);
+
+        //act
+
+        //assert
+        assertThat(exercisePosition.getId()).isNull();
+        assertThat(exercisePosition.getExercise().getId()).isNull();
+        assertThat(exercisePosition.getTemplate().getId()).isNull();
+
+    }
 }
